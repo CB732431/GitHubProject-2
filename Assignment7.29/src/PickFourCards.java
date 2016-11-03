d0fd386870f8abc3ab6d38c0524e4a92785982b8
CB732431 <beardc@student.swosu.edu>
2016-11-02 19:32:48 -0500

    added custom method.

--- Assignment7.29/src/PickFourCards.java
+++ Assignment7.29/src/PickFourCards.java
@@ -6,6 +6,15 @@ public class PickFourCards {
 		// input code
 		
 		//make custom method
+public static int pickCard() {
+  int card = (int) (Math.random() * 52.0); // pick a card 0-51
+  return card;
+ }
+ 
+ public static int getRank(int card) {
+ 
+  return card / 4 + 1; // determine the rank 1-13
+ 
 	}
 
 }
